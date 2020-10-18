32ff38800ca2d62252e619bf3414c5675fa4dc25
pdz <pdz@pdz.me>
2020-10-18 21:29:11 +0300

    deploy

---
+++ precache-manifest.5e0b88be0501a982bb03be7b8dcf56f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23b0edf1070321a2ec48",
+    "url": "/css/app.dd3adc11.css"
+  },
+  {
+    "revision": "12e069a980108a05e87133bbecde7125",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "1aee4af649096f80126725cde54a896a",
+    "url": "/img/logo.svg"
+  },
+  {
+    "revision": "ef586702186cfcf6eb94d18733420944",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b0edf1070321a2ec48",
+    "url": "/js/app.bd2b8333.js"
+  },
+  {
+    "revision": "46cebd71723635a3185a",
+    "url": "/js/chunk-vendors.ca84d1a6.js"
+  },
+  {
+    "revision": "ba94226ba79e18207edfa9ee4419dc57",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b4a169d0ce625909423d2e58fe46aee9",
+    "url": "/qr-scanner-worker.min.js"
+  }
+]);
